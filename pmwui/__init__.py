6964fd94d8d5eb155b62e01f09fdfebdcd526a3b
Rob Ellis <rob.ellis@jic.ac.uk>
2025-02-17 15:55:00 +0000

    WIP

--- pmwui/__init__.py
+++ pmwui/__init__.py
@@ -1,3 +1,27 @@
 def main():
     """Entry point for the application script"""
     print("Call your main application code here!!!!!!!!")
+
+
+from flask import Flask
+
+# Create the Flask application
+app = Flask(__name__)
+
+# Define a route for the home page
+@app.route('/')
+def hello_world():
+    return 'Hello, World!!!E!!!!'
+
+# Define another route for a different page
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello, {name}!'
+
+# Add a main function to run the Flask app via the command line
+def main2():
+    app.run(debug=True)
+
+# Run the application when the script is executed directly
+if __name__ == "__main__":
+    main2()

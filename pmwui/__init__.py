f156916e2caad7cf3fb0eb37919fa1d10390eb2b
Rob Ellis <rob.ellis@jic.ac.uk>
2025-02-20 13:04:32 +0000

    add the beginings of a cli and a file for db stuff

--- pmwui/__init__.py
+++ pmwui/__init__.py
@@ -1,27 +1,38 @@
-def main():
-    """Entry point for the application script"""
-    print("Call your main application code here!!!!!!!!")
-
+import sys
 
 from flask import Flask
 
 # Create the Flask application
 app = Flask(__name__)
 
+
 # Define a route for the home page
 @app.route('/')
 def hello_world():
     return 'Hello, World!!!E!!!!'
+
 
 # Define another route for a different page
 @app.route('/greet/<name>')
 def greet(name):
     return f'Hello, {name}!'
 
+
 # Add a main function to run the Flask app via the command line
 def main2():
     app.run(debug=True)
 
-# Run the application when the script is executed directly
+
+def main():
+    print(sys.argv)
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "add":
+            print("add genome")
+            exit(0)
+
+    print("run app?")
+    app.run(debug=True)
+
+
 if __name__ == "__main__":
-    main2()
+    main()

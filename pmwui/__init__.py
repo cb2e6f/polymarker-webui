735de4783809e99cb9e5b6308f0da89055e0737f
Rob Ellis <rob.ellis@jic.ac.uk>
2025-02-20 16:15:35 +0000

    add some initial indexing and db importing stuff

--- pmwui/__init__.py
+++ pmwui/__init__.py
@@ -1,5 +1,8 @@
+import subprocess
 import sys
 
+import mariadb
+import yaml
 from flask import Flask
 
 # Create the Flask application
@@ -17,20 +20,122 @@ def hello_world():
 def greet(name):
     return f'Hello, {name}!'
 
+def index(path):
+    print(f"path ===== {path}")
+    print("indexing")
+    command = f"samtools faidx {path}"
+    result = subprocess.run(command, shell=True)
+    if result.returncode > 0:
+        print(result)
+        if result.returncode == 127:
+            print("samtools not found")
+        elif result.returncode == 1:
+            print(f"failed to open file {path}")
+        exit(result.returncode)
 
-# Add a main function to run the Flask app via the command line
-def main2():
-    app.run(debug=True)
+    print("-------------")
 
+    command = f"makeblastdb -dbtype 'nucl' -in {path} -out {path}"
+    result = subprocess.run(command, shell=True)
+    if result.returncode > 0:
+        print("raise hell")
+        print(result)
+        exit(-1)
+    print("-------------")
+
+
+def connect():
+    try:
+        connection = mariadb.connect(
+            user="re",
+            password="",
+            host="localhost",
+            port=3306,
+            database="cmd_queue_db"
+        )
+        return connection
+    except mariadb.Error as e:
+        print(f"error connecting to mariadb: {e}")
+        return None
+
+
+def create_reference_table(connection):
+    cursor = connection.cursor()
+
+    query = """
+    CREATE TABLE IF NOT EXISTS reference (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        name TEXT,
+        path TEXT,
+        genome_count TEXT,
+        arm_selection TEXT,
+        description TEXT,
+        example TEXT        
+    );
+    """
+
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print(f"created reference table: {query}")
+    except mariadb.Error as e:
+        print(f"error creating reference table: {e}")
+
+
+def insert_reference(connection, config):
+    cursor = connection.cursor()
+
+    query = """
+    INSERT INTO reference (name, path, genome_count, arm_selection, description, example)
+    VALUES (?, ?, ?, ?, ?, ?)
+    """
+
+    try:
+        cursor.execute(query, (
+            config["name"], config["path"], config["genome_count"], config["arm_selection"], config["description"],
+            config["example"]))
+        connection.commit()
+        print(f"inserted reference: {query}")
+    except mariadb.Error as e:
+        print(f"error inserting reference: {e}")
+
+
+
+
+def add(path):
+
+    try:
+        reference_data_file = open(path)
+        try:
+            reference_data = yaml.safe_load(reference_data_file)
+            print("-------")
+            try:
+                index(reference_data[0]["path"])
+                db_connection = connect()
+
+                if db_connection is not None:
+                    create_reference_table(db_connection)
+                    insert_reference(db_connection, reference_data[0])
+            except KeyError:
+                print(f"missing path value from reference yaml")
+                exit(-1)
+        except yaml.parser.ParserError:
+            print(f"file {path} does not appear to be a valid yaml")
+            reference_data_file.close()
+            exit(-1)
+        reference_data_file.close()
+    except FileNotFoundError:
+        print(f"file {path} not found")
+        exit(-1)
 
 def main():
-    print(sys.argv)
     if len(sys.argv) > 1:
         if sys.argv[1] == "add":
-            print("add genome")
+            if len(sys.argv) == 3:
+                add(sys.argv[2])
+            else:
+                print("invalid options")
             exit(0)
-
-    print("run app?")
     app.run(debug=True)
 
 
